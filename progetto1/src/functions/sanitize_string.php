f651b4e739de531236b413b71d6ecbc370c9f0c8
aelio <60773051+aelio0@users.noreply.github.com>
2023-04-11 21:14:42 +0200

    Delete progetto1 directory

--- progetto1/src/functions/sanitize_string.php
+++
@@ -1,7 +0,0 @@
-<?php
-function sanitize_string($data){
-    $data = trim($data);                   //rimuove gli spazi prima e dopo la stringa 
-    $data = stripslashes($data);    // rimuove gli slashes 
-    $data=(filter_var($data, FILTER_SANITIZE_STRING));
-    return $data;
-}
